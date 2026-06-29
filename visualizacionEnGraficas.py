6ef63e0643c9dae06ceb84d81503fb784e597149
David19 <davidleonel911@gmail.com>
2026-06-28 20:16:15 -0500

    grafico de dispersion para ver la correlacion entre las horas de estudio y las calificaciones

--- visualizacionEnGraficas.py
+++ visualizacionEnGraficas.py
@@ -24,4 +24,18 @@ plt.ylabel('Cantidad de Estudiantes') #etiqueta del eje y
 plt.show()#muestra la grafica
 
 """
+"""
+    #Dispersion 
+plt.figure(figsize=(8, 5))
+
+# sns.scatterplot es la función para puntos de dispersión
+# (útil cuando tienes 25,000 puntos para que no se vea todo negro)
+#las variables x y son las columnas que vayamos a comparar, alpha=0.5: Hace que los puntos sean un poco transparentes 
+sns.scatterplot(data=df, x='study_hours', y='overall_score')
+
+plt.title('Relación entre Horas de Estudio y Calificación')
+plt.xlabel('Horas de Estudio Diarias')
+plt.ylabel('Puntuación General')
+plt.show()
+"""
 
